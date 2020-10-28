38cec8e6f619a7f90c407488f3c9c5e30dd81ff3
wanghuaxiang <626797062@qq.com>
2020-10-28 20:33:07 +0800

    添加nacos config

--- payserver/src/main/java/com/wanghuaxiang/whxmall/payserver/controller/PayController.java
+++ payserver/src/main/java/com/wanghuaxiang/whxmall/payserver/controller/PayController.java
@@ -4,6 +4,8 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.wanghuaxiang.whxmall.payserver.handler.SentinelBlockHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020-10-22 14:56
  * @Description 支付管理
  */
+@RefreshScope //让nacos上的配置能实时刷新
 @RestController
 @RequestMapping("/needPay")
 public class PayController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${myconfig.test}")
+    private String configTest;
 
     @GetMapping("wxPay")
 //    @SentinelResource(
@@ -28,6 +34,6 @@ public class PayController {
     public String wxPay(int money) {
         logger.info("wxPay is request");
         int a = 100 / money;
-        return "wx pay money num = " + money;
+        return "wx pay money num = " + money +" and configTest=" + configTest;
     }
 }
